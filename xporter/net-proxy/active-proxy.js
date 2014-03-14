418ad3c724ef57e16148c5edff54420fda1868a7
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-03-15 07:35:06 +0900

    use chan select

--- xporter/net-proxy/active-proxy.js
+++ xporter/net-proxy/active-proxy.js
@@ -33,52 +33,85 @@ function* newSoc() {
   console.log('passive port connected');
 
   var svrSoc;
+  var chCliRead = chan();
+  var chCliEnd  = chan();
+  var chCliErr  = chan();
+  var chSvrEnd  = chan();
+  var chSvrErr  = chan();
 
   //cliSoc.pipe(svrSoc);
-  cliSoc.on('readable', co(function* () {
-    var buff = cliSoc.read();
-    if (buff) {
-      if (!svrSoc) {
-        console.log('proxy port connecting');
-        try {
-          svrSoc = yield net_connect(PROXY_PORT, PROXY_HOST);
-        } catch (err) {
-          console.log('proxy port connect fail: ' + err);
-          process.exit(1);
-        }
-        console.log('proxy port connected');
+  cliSoc.on('readable', chCliRead);
+  cliSoc.on('end',      chCliEnd);
+  cliSoc.on('error',    chCliErr);
 
-        svrSoc.pipe(cliSoc);
-        //svrSoc.on('readable', function () {
-        //  var buff = svrSoc.read();
-        //  if (buff) cliSoc.write(buff);
-        //});
-        svrSoc.on('end', function () {
-          //cliSoc.end();
-          console.log('end of passive port');
-          co(newSoc)();
-        });
+  var bCliAlive = true;
+  var bSvrAlive = false;
 
-        svrSoc.on('error', function (err) {
-          cliSoc.end();
-          console.log('%s %s: %s', new Date().toLocaleTimeString(),
-            'svrSoc', err);
-        });
+  while (bCliAlive || bSvrAlive) {
 
-      } // if !svrSoc
-      svrSoc.write(buff);
-    }
-  }));
-  cliSoc.on('end', function () {
-    if (svrSoc) svrSoc.end();
-    console.log('end of proxy port');
-  });
+    switch (yield chan.select(
+              chCliRead, chCliEnd, chCliErr, chSvrEnd, chSvrErr)) {
 
-  cliSoc.on('error', function (err) {
-    if (svrSoc) svrSoc.end();
-    console.log('%s %s: %s', new Date().toLocaleTimeString(),
-      'cliSoc', err);
-  });
+    case chCliRead:
+      yield chCliRead.selected;
+      var buff = cliSoc.read();
+      if (buff) {
+        if (!svrSoc) {
+          console.log('proxy port connecting');
+          try {
+            svrSoc = yield net_connect(PROXY_PORT, PROXY_HOST);
+            bSvrAlive = true;
+          } catch (err) {
+            console.log('proxy port connect fail: ' + err);
+            process.exit(1);
+          }
+          console.log('proxy port connected');
+
+          svrSoc.pipe(cliSoc);
+          //svrSoc.on('readable', function () {
+          //  var buff = svrSoc.read();
+          //  if (buff) cliSoc.write(buff);
+          //});
+          svrSoc.on('end', chSvrEnd);
+          svrSoc.on('error', chSvrErr);
+        } // if !svrSoc
+        svrSoc.write(buff);
+      }
+      break;
+
+    case chCliEnd:
+      yield chCliEnd.selected;
+      if (svrSoc) svrSoc.end();
+      console.log('end of proxy port');
+      bCliAlive = false;
+      break;
+
+    case chCliErr:
+      var err = yield chCliErr.selected;
+      if (svrSoc) svrSoc.end();
+      console.log('%s %s: %s', new Date().toLocaleTimeString(),
+        'cliSoc', err);
+      break;
+
+    case chSvrEnd:
+      yield chSvrEnd.selected;
+      //cliSoc.end();
+      console.log('end of passive port');
+      co(newSoc)();
+      bSvrAlive = false;
+      break;
+
+    case chSvrErr:
+      var err = yield chSvrErr.selected;
+      cliSoc.end();
+      console.log('%s %s: %s', new Date().toLocaleTimeString(),
+        'svrSoc', err);
+      break;
+    default:
+      throw new Error('ここには来ない');
+      break;
+    } // switch chan
+  } // while loop
 } // newSoc
 
 for (var i = 0; i < 100; ++i)
